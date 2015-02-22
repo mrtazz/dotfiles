8f28f7f4608f0fd2d8ecc9341b30fb5fa8dfa6d6
Daniel Schauenberg <d@unwiredcouch.com>
2015-02-21 19:19:25 -0500

    use platform independent go binary in zsh setup

--- go.zsh
+++ go.zsh
@@ -1,11 +1,11 @@
 # test if we have go installed and want to set up the env
-which go 2> /dev/null
+GO=$(which go 2> /dev/null)
 rc=$?
 if [[ $rc -eq 0 ]]; then
 
   export GOPATH=$HOME/development/go
   if [ ! -d $GOPATH ] ;then mkdir -p $GOPATH ; fi
-  export GOROOT=`/usr/local/bin/go env GOROOT`
+  export GOROOT=`${GO} env GOROOT`
 
   # Customize to your needs...
   export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
