da38a863b5e215d39bebc25f0297e15d4ccc0858
Daniel Schauenberg <d@unwiredcouch.com>
2015-09-12 16:38:18 -0400

    tidy up setting of $GOROOT
    This cleans out $GOROOT/bin from existing $PATH. This is mostly so when go is
    upgraded, it doesn't keep the old path in there in tmux. In order to remove
    the subpath we use the array representation $path to remove the matching
    entry. Also unset $GOROOT after so it can be reset further down.  We don't do
    anything with $GOPATH as it's not dependent on the go version.

--- go.zsh
+++ go.zsh
@@ -3,11 +3,22 @@ GO=$(which go 2> /dev/null)
 rc=$?
 if [[ $rc -eq 0 ]]; then
 
-  export GOPATH=$HOME/development/go
+  # clean out $GOROOT/bin from existing $PATH. This is mostly so when go is
+  # upgraded, it doesn't keep the old path in there in tmux. In order to
+  # remove the subpath we use the array representation $path to remove the
+  # matching entry. Also unset $GOROOT after so it can be reset further down.
+  # We don't do anything with $GOPATH as it's not dependent on the go version.
+  path=( "${path[@]/${GOROOT}/bin}" )
+  unset GOROOT
+
+  export GOPATH=$HOME/code/go
   if [ ! -d $GOPATH ] ;then mkdir -p $GOPATH ; fi
-  export GOROOT=`${GO} env GOROOT`
+  export GOROOT=$(${GO} env GOROOT)
 
   # Customize to your needs...
   export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 fi
+
+# let's try the vendor stuff
+export GO15VENDOREXPERIMENT=1
