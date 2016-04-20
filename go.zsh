4f2e97c5b5def350b98a20104d8251efe41f7c11
Daniel Schauenberg <d@etsy.com>
2016-04-20 16:25:49 -0400

    remove GOROOT setup
    the go binary should take care of this and there shouldn't be any need for the
    user to adapt this.

--- go.zsh
+++ go.zsh
@@ -3,23 +3,11 @@ GO=$(which go 2> /dev/null)
 rc=$?
 if [[ $rc -eq 0 ]]; then
 
-  # clean out $GOROOT/bin from existing $PATH if it's already set. This is
-  # mostly so when go is upgraded, it doesn't keep the old path in there in
-  # tmux. In order to remove the subpath we use the array representation $path
-  # to remove the matching entry. Also unset $GOROOT after so it can be reset
-  # further down.  We don't do anything with $GOPATH as it's not dependent on
-  # the go version.
-  if [[ -n ${GOROOT} ]]; then
-    path=( "${path[@]/${GOROOT}/bin}" )
-    unset GOROOT
-  fi
-
   export GOPATH=$HOME/code/go
   if [ ! -d $GOPATH ] ;then mkdir -p $GOPATH ; fi
-  export GOROOT=$(${GO} env GOROOT)
 
   # Customize to your needs...
-  export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+  export PATH=$GOPATH/bin:$PATH
 
 fi
 
