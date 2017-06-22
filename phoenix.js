46901454ed22a615757df7c835149e27147be5c8
Daniel Schauenberg <d@unwiredcouch.com>
2017-06-22 11:06:10 -0400

    remove bottomLeft phoenix binding
    this only really worked on a big screen and is kinda unnecessary with
    the new quarterscreen bindings

--- phoenix.js
+++ phoenix.js
@@ -115,28 +115,10 @@ var quarterScreen = function (position) {
   }
 }
 
-var bottomLeft = function () {
-
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
-
-  if (window) {
-    window.setTopLeft({
-      x: screen.x,
-      y: screen.y + screen.height * 0.65
-    });
-    window.setSize({
-      width: screen.width * 0.33,
-      height: screen.height * 0.33
-    });
-  }
-}
-
 Key.on('c', mash, center);
 Key.on('l', mash, lefthalf);
 Key.on('r', mash, righthalf);
 Key.on('f', mash, fullscreen);
-Key.on('e', mash, bottomLeft);
 Key.on('q', mash, function() { quarterScreen('lefttop'); });
 Key.on('w', mash, function() { quarterScreen('righttop'); });
 Key.on('a', mash, function() { quarterScreen('leftbottom'); });
