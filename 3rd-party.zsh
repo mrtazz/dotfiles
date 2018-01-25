a752de4990d7b298d7313b8e735a75c9bc5db114
Daniel Schauenberg <d@etsy.com>
2018-01-25 18:06:59 +0100

    add gcloud to 3rd party config

--- 3rd-party.zsh
+++ 3rd-party.zsh
@@ -19,7 +19,7 @@ fi
 [ -f ${HOME}/.dotoverrides/zshrc ] && source ${HOME}/.dotoverrides/zshrc
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/mrtazz/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/mrtazz/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '/Users/dschauenberg/.gcloud/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/dschauenberg/.gcloud/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/mrtazz/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/mrtazz/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '/Users/dschauenberg/.gcloud/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/dschauenberg/.gcloud/google-cloud-sdk/completion.zsh.inc'; fi
