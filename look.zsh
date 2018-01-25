8f14272f5b9d3c7ee7f623ba429c6958ee0aaab6
Daniel Schauenberg <d@etsy.com>
2018-01-25 18:03:16 +0100

    add k8s prompt functions to look

--- look.zsh
+++ look.zsh
@@ -39,7 +39,31 @@ parse_git_dirty() {
   fi
 }
 
-PROMPT='%{$fg_bold[red]%}%m%{$reset_color%}:%{$fg[cyan]%}%c%{$reset_color%}%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}%:%# '
+# k8s stuff
+_kubectl-installed() {
+which kubectl > /dev/null 2>&1
+}
+
+function kube_context_prompt() {
+  if _kubectl-installed && [ -n "${SHOW_K8S_PROMPT}" ]; then
+    echo "[k8s:%{$fg_bold[cyan]%}$(kubectl config current-context)]%{$reset_color%}"
+  fi
+}
+
+function k8s_toggle_prompt() {
+  if [ -z "${SHOW_K8S_PROMPT}" ]; then
+    export SHOW_K8S_PROMPT=true
+  else
+    unset SHOW_K8S_PROMPT
+  fi
+}
+
+ORIG_PROMPT=$PROMPT
+set_prompt() {
+  PROMPT="%{$fg_bold[white]%}%2m%{$reset_color%} $(kube_context_prompt) ${ORIG_PROMPT}"
+}
+
+PROMPT='%{$fg_bold[red]%}%m%{$reset_color%}:%{$fg[cyan]%}%c%{$reset_color%}%{$fg_bold[green]%}$(git_prompt_info)$(kube_context_prompt)%{$reset_color%}%:%# '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="[%{$fg[red]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
