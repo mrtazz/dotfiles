438ec5fe271dcec72abc9de84d5496768af0a1cd
Daniel Schauenberg <d@unwiredcouch.com>
2021-01-13 18:38:31 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is for compatibility with GitHub codespaces:
+# https://docs.github.com/en/free-pro-team@latest/github/developing-online-with-codespaces/personalizing-codespaces-for-your-account
+
+make install
