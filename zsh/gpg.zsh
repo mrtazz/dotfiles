2e0399034dbfd3d7cb521ee5a45d6bf27cbab029
Daniel Schauenberg <mrtazz@github.com>
2021-06-02 10:08:35 +0200

    --write-env-file is deprecated and doesn't do anything

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -12,5 +12,5 @@ if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
   source ~/.gnupg/.gpg-agent-info
   export GPG_AGENT_INFO
 else
-  eval $(/usr/local/bin/gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+  eval $(/usr/local/bin/gpg-agent --daemon)
 fi
