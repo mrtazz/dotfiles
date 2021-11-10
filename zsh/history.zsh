368b11d683aedc1ef905210bf69064633250efa3
Daniel Schauenberg <d@unwiredcouch.com>
2021-11-10 13:30:04 +0100

    Update history.zsh

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,10 +1,12 @@
 # history settings
-HISTFILE=$HOME/.zsh_history
+HISTFILE=$HOME/.zsh/history
 HISTSIZE=10000
 SAVEHIST=10000
 
 setopt append_history
+setopt inc_append_history
 setopt extended_history
+setopt share_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
