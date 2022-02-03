ea7260af6ca843ba431ed4afc6ee68c87910f3b5
Daniel Schauenberg <mrtazz@github.com>
2022-02-03 11:55:18 +0100

    don't overwrite default grep

--- zsh/aliases_functions.zsh
+++ zsh/aliases_functions.zsh
@@ -8,5 +8,5 @@ function reload_gpg_agent() {
 alias irc='set_iTerm_title irc;mosh portal.unwiredcouch.com -- tmux attach -d -t werk'
 alias mutt='MUTT_IDENTITY=~/.dotoverrides/identity-unwiredcouch /usr/local/bin/mutt'
 alias fcd="cd \$(FZF_DEFAULT_COMMAND='fd --type directory' fzf --height 40%)"
-alias grep='grep -R -i --exclude=tags --color=auto'
+alias ggrep='grep -R -i --exclude=tags --color=auto'
 alias cssh='gh cs ssh'
