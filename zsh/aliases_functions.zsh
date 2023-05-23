5327538659485c7a752b0e7e1905bab085cfdd24
Daniel Schauenberg <d@unwiredcouch.com>
2023-05-23 10:48:30 +0200

    exclude binary files from grep alias

--- zsh/aliases_functions.zsh
+++ zsh/aliases_functions.zsh
@@ -8,5 +8,5 @@ function reload_gpg_agent() {
 alias irc='set_iTerm_title irc;mosh portal.unwiredcouch.com -- tmux attach -d -t werk'
 alias mutt='MUTT_IDENTITY=~/.dotoverrides/identity-unwiredcouch mutt'
 alias fcd="cd \$(FZF_DEFAULT_COMMAND='fd --type directory' fzf --height 40%)"
-alias ggrep='grep -R -i --exclude=tags --color=auto'
+alias ggrep='grep -R -I -i --exclude=tags --color=auto'
 alias cssh='gh cs ssh'
