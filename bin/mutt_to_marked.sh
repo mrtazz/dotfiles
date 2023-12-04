b44e838bd55174114f3c4f929563690dae2093b6
Daniel Schauenberg <d@unwiredcouch.com>
2023-12-04 17:32:15 +0100

    add some comments to mutt_to_marked.sh

--- bin/mutt_to_marked.sh
+++ bin/mutt_to_marked.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 #
+# small utility to view an email message containing markdown rendered in the
+# Marked 2 app on macOS from mutt. You can use it from mutt with the following
+# example macro '.m' from the index or pager:
+#
+# macro index,pager .m "<enter-command>set wait_key=no<enter><enter-command>set pipe_decode=yes<enter><pipe-entry>~/bin/mutt_to_marked.sh<enter>" 'Open entry in Marked app'
+#
+# Caveat: this will leave the last email viewed as a tempfile on your computer
+# in $TMPDIR. This is up to you to decide whether it's a privacy issue or not.
 
 
 TMPFILE_PREFIX="mutt_to_marked"
 
 # first clean up any leftover files
-
 rm -f "${TMPDIR}/${TMPFILE_PREFIX}"*
 
 tmpfile=$(mktemp -t "${TMPFILE_PREFIX}")
