9d066edf38179fbf4429601a2077b7d794044382
Daniel Schauenberg <d@unwiredcouch.com>
2023-12-27 18:13:08 +0100

    add cargo env to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH=~/bin:/usr/local/bin:/usr/local/sbin:/home/linuxbrew/.linuxbrew/bin/:/opt/homebrew/bin:$PATH
+export PATH=~/.cargo/bin:~/bin:/usr/local/bin:/usr/local/sbin:/home/linuxbrew/.linuxbrew/bin/:/opt/homebrew/bin:$PATH
