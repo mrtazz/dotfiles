0eeebb9fc648f332f619cceaff081365e13d7cbc
Github <actions@noreply.github.com>
2025-04-16 03:41:11 +0000

    update vim plugins

--- config/vim/pack/plugins/start/fzf/src/terminal.go
+++ config/vim/pack/plugins/start/fzf/src/terminal.go
@@ -2647,6 +2647,9 @@ func (t *Terminal) headerIndent(borderShape tui.BorderShape) int {
 	}
 	if borderShape.HasLeft() {
 		indentSize -= 1 + t.borderWidth
+		if indentSize < 0 {
+			indentSize = 0
+		}
 	}
 	return indentSize
 }
@@ -3144,7 +3147,7 @@ func (t *Terminal) printHighlighted(result Result, colBase tui.ColorPair, colMat
 			wasWrapped = true
 		}
 
-		if len(line) > 0 && line[len(line)-1] == '\n' {
+		if len(line) > 0 && line[len(line)-1] == '\n' && lineOffset < len(lines)-1 {
 			line = line[:len(line)-1]
 		} else {
 			wrapped = true
