d867cb4461d0b6b8a24ff29b6930446f8c935d84
Daniel Schauenberg <d@unwiredcouch.com>
2026-06-30 11:41:08 +0200

    add fzf based tmux switcher

---
+++ config/tmux/plugins/tmux-switch-session/tmux-switch-session.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# fuzzy switching tmux sessions.
+# use with: bind-key S run-shell -b "~/.tmux/tmux-switch-session.sh"
+# taken from https://jdhao.github.io/2021/11/20/tmux_fuzzy_session_switch/
+tmuxsessions=$(tmux list-sessions -F "#{session_name}")
+
+tmux_switch_to_session() {
+    session="$1"
+    if [[ $tmuxsessions = *"$session"* ]]; then
+        tmux switch-client -t "$session"
+    fi
+}
+
+choice=$(sort -rfu <<< "$tmuxsessions" \
+    | fzf-tmux \
+    | tr -d '\n')
+tmux_switch_to_session "$choice"
