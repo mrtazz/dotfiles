2b3204d4124b3a1517da007b69884bfccacd9eef
Daniel Schauenberg <d@unwiredcouch.com>
2026-07-21 11:10:57 +0200

    combine tmux use cases in helper function

--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -2,7 +2,25 @@
 
 # aliases
 alias tma='tmux attach -d -t'
-alias tmn='tmux new -s $(basename ${PWD})'
+
+# tmux helper to attach a session if there is one or start a new one if there
+# isn't
+function tmx() {
+  sessions=$(tmux list-sessions -F '#S' 2>/dev/null)
+  session_count=$(echo ${sessions} | wc -l)
+
+  if [ ${session_count} -eq 1 ]; then
+    # base case we have one session to attach to
+    tmux attach -d
+  elif [ ${session_count} -gt 1 ]; then
+    # there is more than one session so let's choose one
+    chosen_session=$(echo ${sessions} | fzf)
+    tmux attach -d -t ${chosen_session}
+  else
+    # no sessions exist so we create a new one
+    tmux new -s $(basename ${PWD})
+  fi
+}
 
 # set colors properly
 [ -n "$TMUX" ] && export TERM=screen-256color
