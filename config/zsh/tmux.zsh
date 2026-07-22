e71ca616f7727024e155c5d365c2d2437d5a9d38
Daniel Schauenberg <d@unwiredcouch.com>
2026-07-22 10:49:10 +0200

    improve tmx logic

--- config/zsh/tmux.zsh
+++ config/zsh/tmux.zsh
@@ -7,20 +7,24 @@ alias tma='tmux attach -d -t'
 # isn't
 function tmx() {
   sessions=$(tmux list-sessions -F '#S' 2>/dev/null)
-  session_count=$(echo ${sessions} | wc -l)
-
-  if [ ${session_count} -eq 1 ]; then
-    # base case we have one session to attach to
-    tmux attach -d
-  elif [ ${session_count} -gt 1 ]; then
-    # there is more than one session so let's choose one
-    chosen_session=$(echo ${sessions} | fzf)
-    tmux attach -d -t ${chosen_session}
-  else
+  if [ -z "${sessions}" ]; then
     # no sessions exist so we create a new one
     tmux new -s $(basename ${PWD})
+  else
+    session_count=$(echo ${sessions} | wc -l)
+
+    if [ ${session_count} -eq 1 ]; then
+      # base case we have one session to attach to
+      tmux attach -d
+    elif [ ${session_count} -gt 1 ]; then
+      # there is more than one session so let's choose one
+      chosen_session=$(echo ${sessions} | fzf)
+      tmux attach -d -t ${chosen_session}
+    else
+    fi
   fi
 }
+
 
 # set colors properly
 [ -n "$TMUX" ] && export TERM=screen-256color
